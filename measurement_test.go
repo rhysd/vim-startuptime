911838fbcb35016af99a60bb4c8c731751d223a0
rhysd <lin90162@yahoo.co.jp>
2018-05-31 11:48:20 +0900

    add tests for formatting summary

---
+++ measurement_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestPrintSummary(t *testing.T) {
+	ave := &averageMeasurement{
+		time.Duration(200) * time.Millisecond,
+		[]averageEntry{
+			averageEntry{
+				"/foo/bar.vim",
+				time.Duration(12345) * time.Microsecond,
+			},
+			averageEntry{
+				"$VIM/vimrc",
+				time.Duration(1234) * time.Microsecond,
+			},
+		},
+	}
+
+	var buf bytes.Buffer
+	ave.printSummary(&buf)
+	lines := strings.Split(buf.String(), "\n")
+
+	if lines[len(lines)-1] != "" {
+		t.Error("Output does not end with newline")
+	}
+	lines = lines[:len(lines)-1]
+
+	if !strings.HasPrefix(lines[0], "Total: 200") {
+		t.Error("Header does not show total average:", lines[0])
+	}
+	lines = lines[2:]
+
+	have := lines
+	want := []string{
+		"12.345000: /foo/bar.vim",
+		"1.234000: $VIM/vimrc",
+	}
+
+	if !reflect.DeepEqual(have, want) {
+		t.Fatalf("Profile result per entry is unexpected. Have '%v' but want '%v'", have, want)
+	}
+}
