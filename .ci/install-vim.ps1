9dc4e1be97de0d4f350a381dec6991f00517573e
rhysd <lin90162@yahoo.co.jp>
2018-06-06 16:03:56 +0900

    fix setting $Path on Windows on AppVeyor

--- .ci/install-vim.ps1
+++ .ci/install-vim.ps1
@@ -4,7 +4,6 @@ $redirect = Invoke-WebRequest -URI 'http://vim-jp.org/redirects/koron/vim-kaoriy
 (New-Object Net.WebClient).DownloadFile($redirect.Links[0].href, $zip)
 [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null
 [System.IO.Compression.ZipFile]::ExtractToDirectory($zip, $vim)
-$Env:Path = $vim + (Get-ChildItem $vim).Name + ';' + $Env:Path
 
 $zip = $Env:APPVEYOR_BUILD_FOLDER + '\nvim-win64.zip'
 $nvim = $Env:APPVEYOR_BUILD_FOLDER + '\nvim-win64\'
@@ -12,4 +11,3 @@ $url = 'https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zi
 (New-Object Net.WebClient).DownloadFile($url, $zip)
 [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null
 [System.IO.Compression.ZipFile]::ExtractToDirectory($zip, $nvim)
-$Env:PATH = $Env:PATH + ';' + $nvim + '\Neovim\bin\'
