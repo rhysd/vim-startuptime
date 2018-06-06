1d4986caba60cdb47bc54c728fccc799f3322ec5
rhysd <lin90162@yahoo.co.jp>
2018-06-06 17:02:22 +0900

    add a test for usage()

--- options_test.go
+++ options_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -16,5 +19,39 @@ func TestParseExtraArgs(t *testing.T) {
 	want := []string{"--cmd", "Foo"}
 	if !reflect.DeepEqual(opts.extraArgs, want) {
 		t.Fatal("Wanted:", want, ", but have:", opts.extraArgs)
+	}
+}
+
+func TestUsage(t *testing.T) {
+	saved := os.Stderr
+	defer func() {
+		os.Stderr = saved
+	}()
+
+	dir, err := ioutil.TempDir("", "__vim_usage_test")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+
+	tempName := filepath.Join(dir, "stderr")
+	temp, err := os.Create(tempName)
+	if err != nil {
+		panic(err)
+	}
+	os.Stderr = temp
+
+	usage()
+
+	temp.Close()
+
+	bytes, err := ioutil.ReadFile(tempName)
+	if err != nil {
+		panic(err)
+	}
+
+	out := string(bytes)
+	if !strings.Contains(out, "Usage: vim-startuptime") {
+		t.Fatal("Unexpected usage output:", out)
 	}
 }
