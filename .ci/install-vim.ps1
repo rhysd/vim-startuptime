222ffe25c43b79ce3526ffd5a4127852d2eae4eb
rhysd <lin90162@yahoo.co.jp>
2018-06-06 15:46:22 +0900

    install Neovim on CI

--- .ci/install-vim.ps1
+++ .ci/install-vim.ps1
@@ -5,3 +5,11 @@ $redirect = Invoke-WebRequest -URI 'http://vim-jp.org/redirects/koron/vim-kaoriy
 [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null
 [System.IO.Compression.ZipFile]::ExtractToDirectory($zip, $vim)
 $Env:Path = $vim + (Get-ChildItem $vim).Name + ';' + $Env:Path
+
+$zip = $Env:APPVEYOR_BUILD_FOLDER + '\nvim-win64.zip'
+$nvim = $Env:APPVEYOR_BUILD_FOLDER + '\nvim-win64\'
+$url = 'https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip'
+(New-Object Net.WebClient).DownloadFile($url, $zip)
+[Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zip, $nvim)
+$Env:PATH = $Env:PATH + ';' + $nvim + '\Neovim\bin\'
